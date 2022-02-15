e2ab6f1aea47fefb5b4d7c61412d3a7a1b1c33e5
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2022-02-15 21:35:15 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\studentsController;
+use App\Http\Middleware\VerifyCsrfToken;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('/students')->group(function () {
+    Route::get('/{id}',[ studentsController::class,'get']);
+    Route::post('/',[ studentsController::class,'post']);
+    Route::put('/{id}',[ studentsController::class,'update']);
+    Route::delete('/{id}',[ studentsController::class,'delete']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
